d4c49a8bd943000ef6d024995fcf10ce85ea4d99
Jonathan Holmes <Vorlias@users.noreply.github.com>
2018-05-07 13:55:35 +1200

    Add JSON, started Http lib

--- RasaLib/HTTP/Http.cs
+++ RasaLib/HTTP/Http.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace RasaLib.HTTP
 {
@@ -11,6 +15,27 @@ namespace RasaLib.HTTP
     /// </summary>
     internal class Http
     {
+        public static void PostJson<T>(string uri, T data)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
 
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = JObject.FromObject(data).ToString();
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
     }
 }
