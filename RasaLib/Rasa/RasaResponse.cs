4a399a000e77c124814557db27bfc2182b5c6aa1
Jonathan Holmes <Vorlias@users.noreply.github.com>
2018-05-07 14:15:39 +1200

    Added RasaIntent + RasaResponse

---
+++ RasaLib/Rasa/RasaResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+using RasaLib.Rasa.Response;
+
+namespace RasaLib.Rasa
+{
+    public class RasaResponse
+    {
+        public RasaIntent Intent { get; internal set;}
+        public JObject Entities { get; internal set; }
+    }
+}
