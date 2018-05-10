46a675e69a0a2d022ef7fe0df2c1b5bd8bd5823b
Jonathan Holmes <Vorlias@users.noreply.github.com>
2018-05-10 16:59:45 +1200

    Adding changes

--- BotPrototypeCLI/BotPrototypeCLI/Program.cs
+++ BotPrototypeCLI/BotPrototypeCLI/Program.cs
@@ -7,14 +7,43 @@ using System.Threading.Tasks;
 
 namespace BotPrototypeCLI
 {
+    class Paper
+    {
+        public string Name { get; set; }
+        public string[] Aliases { get; set; }
+        public string[] Requirements { get; set; }
+        public string PaperCode { get; set; }
+
+        public Paper(string paperCode, string name, params string[] aliases)
+        {
+            Name = name;
+            PaperCode = paperCode;
+            Aliases = aliases;
+        }
+    }
+
     class Program
     {
+        static Paper[] papersTest = new Paper[] {
+            new Paper("ENSE701", "Contemporary Methods in Software Engineering", "Software Engineering") {Requirements=new string[]{ "COMP603" } },
+            new Paper("COMP603", "Program Design and Construction", "PDC")
+        };
+
+        static void WriteOutput(string message)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Veronica: {0}", message);
+            Console.ForegroundColor = oldColor;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello there!");
             string input = string.Empty;
             do
             {
-                Console.Write("Input> ");
+                Console.Write("# ");
                 input = Console.ReadLine();
 
                 if (input != string.Empty)
@@ -22,25 +51,36 @@ namespace BotPrototypeCLI
                     RasaQuery rasaQuery = new RasaQuery(input);
                     var result = rasaQuery.GetResponse();
 
-                    if (result.Intent != null)
-                        Console.WriteLine("Got: {0} @{1}% confidence", result.Intent.Name, result.Intent?.Confidence * 100f);
-                    else
-                        Console.WriteLine("Got no intents.");
-
-                    foreach (var entity in result.Entities)
+                    if (result.Intent.Name == "paper_requirements")
                     {
-                        Console.WriteLine("\t{0}", entity);
-                    }
-
-                    Console.WriteLine("Other potential results:  ");
-                    foreach (var intent in result.IntentRankings)
-                    {
-                        Console.WriteLine("\tGot: {0} @{1}% confidence", intent.Name, intent.Confidence * 100);
-                        foreach (var entity in result.Entities)
+                        var papers = result.Entities.Where(entity => entity.Entity == "paper").Select(entity => entity.Value);
+                        if (papers.Contains("software engineering"))
                         {
-                            Console.WriteLine("\t\t{0}", entity);
+                            WriteOutput("To do Contemporary Methods in Software Engineering, you are required to do either COMP603, COMP610 or ENSE600");
                         }
+                        
                     }
+
+
+                    //if (result.Intent != null)
+                    //    Console.WriteLine("Got: {0} @{1}% confidence", result.Intent.Name, result.Intent?.Confidence * 100f);
+                    //else
+                    //    Console.WriteLine("Got no intents.");
+
+                    //foreach (var entity in result.Entities)
+                    //{
+                    //    Console.WriteLine("\t{0}", entity);
+                    //}
+
+                    //Console.WriteLine("Other potential results:  ");
+                    //foreach (var intent in result.IntentRankings)
+                    //{
+                    //    Console.WriteLine("\tGot: {0} @{1}% confidence", intent.Name, intent.Confidence * 100);
+                    //    foreach (var entity in result.Entities)
+                    //    {
+                    //        Console.WriteLine("\t\t{0}", entity);
+                    //    }
+                    //}
                 }
             }
             while (input != string.Empty);
---
+++ RasaLib/Metadata/Paper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasaLib.Metadata
+{
+    public class Paper
+    {
+        public string Name { get; set; }
+        public string[] Aliases { get; set; }
+        public string[] Requirements { get; set; }
+        public string PaperCode { get; set; }
+
+        public Paper(string paperCode, string name, params string[] aliases)
+        {
+            Name = name;
+            PaperCode = paperCode;
+            Aliases = aliases;
+        }
+    }
+}
