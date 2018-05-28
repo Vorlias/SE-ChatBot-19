8ef1e9646d2db4a497c3f98f09714421e0aecbbd
Jonathan Holmes <Vorlias@users.noreply.github.com>
2018-05-28 12:44:14 +1200

    Begin to add paper_suggestion + refactoring

--- Veronica-Web/Controllers/QueryController.cs
+++ Veronica-Web/Controllers/QueryController.cs
@@ -18,6 +18,50 @@ namespace Veronica_Web.Controllers
         Papers papers = new Papers("Data/Papers.json");
 
         /// <summary>
+        /// Gets a result for paper suggestions
+        /// </summary>
+        /// <param name="response">The response from RASA</param>
+        /// <returns>A user-friendly response based on the RASA response</returns>
+        private JsonResult GetPaperSuggestionResult(RasaResponse response)
+        {
+            // TODO: Implement
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a result for paper requirements
+        /// </summary>
+        /// <param name="response">The response from RASA</param>
+        /// <returns>A user-friendly response based on the RASA response</returns>
+        private JsonResult GetPaperRequirementResult(RasaResponse response)
+        {
+            // Get the entities that match 'paper'
+            var paperEntities = response.Entities.Where(entity => entity.Entity == RasaResponse.ENTITY_PAPER);
+            if (paperEntities.Count() > 0)
+            {
+                // Get the first entity, then find it in our paper data
+                var paper = paperEntities.First();
+                var matchingPaper = papers.FindPaperByKeyword(paper.Value);
+
+
+                if (matchingPaper != null) // If we have a matching paper
+                {
+                    var requirementsString = string.Join(" or ", matchingPaper.Requirements);
+
+                    // Respond with the requirements for the paper
+                    return QueryResponse.Result($"{matchingPaper.Name} ({matchingPaper.PaperCode}) requires that you have completed {requirementsString}");
+                }
+                else  // If we don't have the paper in our data, inform the user.
+                {
+
+                    return QueryResponse.Result("I'm sorry, I don't have any information regarding that as of yet.");
+                }
+            }
+            else
+                return QueryResponse.Result("I'm sorry, could you ask that again?");
+        }
+
+        /// <summary>
         /// API end-point for api/Query/Ask (POST)
         /// - Accepts JSON
         /// - Must be in QueryArguments format
@@ -30,55 +74,38 @@ namespace Veronica_Web.Controllers
             if (query.UserInput.StartsWith("@")) // Enable us to debug the bot in case it's displaying incorrect messages
             {
                 RasaQuery rasaQuery = new RasaQuery(query.UserInput.Substring(1));
-                var response = rasaQuery.GetResponse();
+                RasaResponse rasaResponse = rasaQuery.GetResponse();
 
-                return QueryResponse.Result(response.GetDebug());
+                return QueryResponse.Result(rasaResponse.GetDebug());
             }
             else // Regular messages
             {
-                
-                RasaQuery rasaQuery = new RasaQuery(query.UserInput);
-                var response = rasaQuery.GetResponse();
 
-               
-                if (response.Intent == null)  // If for some reason our bot decides there's no intent, we respond gracefully.
+                RasaQuery rasaQuery = new RasaQuery(query.UserInput);
+                RasaResponse rasaResponse = rasaQuery.GetResponse();
+
+
+                if (rasaResponse.Intent == null)  // If for some reason our bot decides there's no intent, we respond gracefully.
                 {
                     return QueryResponse.Result("I'm only able to answer questions in relations to papers at AUT.");
                 }
 
-                
-                if (response.Intent?.Name == RasaResponse.INTENT_REQUIREMENTS) // Check if our intent is paper_requirements
+
+                if (rasaResponse.Intent?.Name == RasaResponse.INTENT_REQUIREMENTS) // Check if our intent is paper_requirements
                 {
-                    // Get the entities that match 'paper'
-                    var paperEntities = response.Entities.Where(entity => entity.Entity == RasaResponse.ENTITY_PAPER);
-                    if (paperEntities.Count() > 0)
-                    {
-                        // Get the first entity, then find it in our paper data
-                        var paper = paperEntities.First();
-                        var matchingPaper = papers.FindPaperByKeyword(paper.Value);
-
-                       
-                        if (matchingPaper != null)  // If we have a matching paper
-                        {
-                            var requirementsString = string.Join(" or ", matchingPaper.Requirements);
-
-                            // Respond with the requirements for the paper
-                            return QueryResponse.Result($"{matchingPaper.Name} ({matchingPaper.PaperCode}) requires that you have completed {requirementsString}");
-                        }
-                        else  // If we don't have the paper in our data, inform the user.
-                        {
-                           
-                            return QueryResponse.Result("I'm sorry, I don't have any information regarding that as of yet.");
-                        }
-                    }
+                    return GetPaperRequirementResult(rasaResponse);
+                }
+                else if (rasaResponse.Intent?.Name == RasaResponse.INTENT_SUGGESTION) // Else if our intent is a paper suggestion
+                {
+                    return GetPaperSuggestionResult(rasaResponse);
                 }
                 else // If our intent isn't supported we need to let the user know
-                { 
+                {
 
                     return QueryResponse.Result("I do not yet have the capability to answer that question.");
                 }
             }
-            
+
             // The last resort response (Should never be reached, hopefully)
             return QueryResponse.Result("I'm sorry, could you be more clear with what you're asking?");
         }
